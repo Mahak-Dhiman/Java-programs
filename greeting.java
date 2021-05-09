d5189898660631716403f3b3e4beb52c5c06f458
Mahak Dhiman <58025022+Mahak-Dhiman@users.noreply.github.com>
2021-05-09 16:38:31 +0530

    Add files via upload

---
+++ greeting.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class main{
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+		String name = new String();
+		System.out.println("Enter your name: ");
+		name = sc.nextLine();
+
+		System.out.println("Hello "+name+", Good morning!");
+
+	}
+}
