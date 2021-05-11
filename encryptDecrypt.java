1ed21a51333a0f296e18b63121bf42f38ae46c68
Mahak Dhiman <58025022+Mahak-Dhiman@users.noreply.github.com>
2021-05-11 12:41:02 +0530

    Add files via upload

---
+++ encryptDecrypt.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class main{
+	public static void main(String[]args){
+		char c = 'A';
+		char encrypt = 'A'+8;
+		System.out.println("Encrypted grade is: "+encrypt);
+		System.out.println("Decrypted grade is: "+c);
+	}
+}
