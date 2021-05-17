3afab0ab9ec5b7fede5f777b2bb778b919b1e36e
Mahak Dhiman <58025022+Mahak-Dhiman@users.noreply.github.com>
2021-05-17 11:22:20 +0530

    Add files via upload

---
+++ arraySum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class main{
+	public static void main(String [] args){
+		float sum =0;
+		float [] a = {1f,2f,3f,4f,5f};
+		for (float e:a){
+			sum=sum+e;
+		}
+		System.out.println("The sum is:"+sum);
+	}
+}
