6e55fc89f40fdd9542b7e4071839ed567cd5d0d3
Mahak Dhiman <58025022+Mahak-Dhiman@users.noreply.github.com>
2021-05-20 11:34:40 +0530

    Add files via upload

---
+++ nthFibo.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class main{
+	static int fibo(int n){
+		if(n==1){
+			return 0;
+		}
+		else if(n==2){
+			return 1;
+		}
+		// or
+		// if(n==1||n==2){
+		// 	return n-1;
+		// }
+		else{
+			return fibo(n-1)+ fibo(n-2);
+		}
+	}
+
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a = fibo(n);
+		System.out.println(a);
+			
+	}
+}
