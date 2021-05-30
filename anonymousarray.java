32f572674e1e24c05009877753579193644baae2
Mahak Dhiman <58025022+Mahak-Dhiman@users.noreply.github.com>
2021-05-30 21:34:48 +0530

    Add files via upload

---
+++ anonymousarray.java
@@ -0,0 +1,12 @@
+class anonymousarr
+{
+	static void printArray(int arr[])
+	{
+		for(int i=0;i<arr.length;i++)
+		System.out.println(arr[i]);
+	}
+	public static void main(String args[])
+	{
+		printArray(new int[]{1,2,3,4,5,6,7,8,9,10});//PASSING ANONYMOUS ARRAY TO METHOD.
+	}
+}
