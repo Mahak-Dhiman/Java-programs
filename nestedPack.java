bc3ee89f10029efee2995ce0413449ee97eba862
Mahak Dhiman <58025022+Mahak-Dhiman@users.noreply.github.com>
2021-06-03 12:13:26 +0530

    Add files via upload

---
+++ nestedPack.java
@@ -0,0 +1,21 @@
+package folder.folder1.folder2;
+class Calculator{
+	void c(){
+		System.out.println("i am c method.");
+	}
+}
+class ScCalculator{
+	void sc(){
+		System.out.println("i am sc method.");
+	}
+}
+class HybridCalculator{
+	void hc(){
+		System.out.println("i am hc method.");
+	}
+}
+class main{
+	public static void main(String [] args){
+		System.out.println("I am main.");
+	}
+}
