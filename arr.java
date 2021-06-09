7c5074b2b4ffcbb587e6341dee6c4e62d9c9efc4
Mahak Dhiman <58025022+Mahak-Dhiman@users.noreply.github.com>
2021-06-09 12:47:34 +0530

    Add files via upload

---
+++ arr.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class main{
+	public static void main(String [] args){
+		ArrayList a = new ArrayList();
+		a.add("S1");
+		a.add("S2");
+		a.add("S3");
+		a.add("S4");
+		a.add("S5");
+		a.add("S6");
+		a.add("S7");
+		a.add("S8");
+		a.add("S9");
+		a.add("S10");
+		System.out.print(a);
+		for(Object e:a){
+			System.out.println(e);
+		}
+	}
+}
