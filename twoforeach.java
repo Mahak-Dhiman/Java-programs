1ca34182c4ebca78f4d7268e5c75486f697c837b
Mahak Dhiman <58025022+Mahak-Dhiman@users.noreply.github.com>
2021-06-17 23:15:51 +0530

    Add files via upload

---
+++ twoforeach.java
@@ -0,0 +1,14 @@
+class twoforeach
+{
+	public static void main(String ags[])
+	{
+		int[][] a={{1,2,3},{4,5}};
+		for (int i=0;i<a.length;i++)
+		{
+			for(int j=0;j<a.length;j++)
+			{
+				System.out.println(a[i][j]);
+			}
+		}
+	}
+}
